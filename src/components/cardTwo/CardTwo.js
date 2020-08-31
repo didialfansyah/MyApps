6820daef3c038c5446d8961a9c7d53e341788867
didialfansyah <didialfansyah@gmail.com>
2020-08-31 20:47:31 +0700

    Sending New

--- src/components/cardTwo/CardTwo.js
+++ src/components/cardTwo/CardTwo.js
@@ -8,7 +8,7 @@ const CardTwo = () => {
             <h2>Next MeetUp</h2>
             <div className="card">
                 <h3>Lorem ipsum dolor sit amet</h3>
-                <a href="#">Getting Start...</a>
+                <a href="https://flatuicolors.com/">Getting Start...</a>
                 <p>
                     Consectetur adipiscing elit. Morbi congue bibendum ex, sit amet iaculis libero pharetra sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus metus eros, vulputate ut vulputate non, convallis a lectus. Praesent ultricies semper dui in feugiat. Sed at hendrerit magna. Nulla dui nisl, efficitur a porttitor at, tincidunt quis dui. Pellentesque lobortis, nisl at semper consectetur, dolor lorem auctor magna, at hendrerit metus orci quis nibh. In dapibus maximus eros aliquet iaculis. Nullam ut faucibus diam, non sollicitudin mauris. Integer accumsan ullamcorper quam quis gravida. In ultrices convallis lacus nec facilisis. Proin facilisis lectus quam, nec dignissim turpis elementum et. Nam blandit dui eros, id tincidunt tellus tempus ac. Nulla id nulla tellus. Phasellus sodales felis at lorem mollis, non auctor felis porta.
                 </p>
